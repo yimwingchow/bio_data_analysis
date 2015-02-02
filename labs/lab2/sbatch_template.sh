8b519e8b70d8e87e86d50ac53114245f960d2ca4
Guillermo Carrasco <guillermo.carrasco@scilifelab.se>
2015-02-02 14:09:49 +0100

    Added sbatch template

---
+++ labs/lab2/sbatch_template.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This is the template sbatch script for Lab2 at BB2490 spring 2015
+
+# Run the script like this:
+# sbatch <OPTIONS> sbatch_template.sh
+# Where the OPTIONS are all of:
+# –A g2015009 –t 2:00:00 –p node –n 8 --res=g2015009_02 –o my_job_name.out
+# (you may change the job name "my_job_name.out" to anything you like).
+
+# Any line that starts with '# ' (dash followed by space) is a comment and will not be executed
+
+# Don't forget to add the appropriate modules! See THE MODULE SYSTEM section in the instructions.
+# bioinfo-tools
+# bwa/0.5.9
+# samtools
+# BEDTools
+
+
+# cd to the directory where the data is (of course, use your own user name):
+cd /home/YOUR_USER_NAME/Lab2
+
+# Then list the commands that you would like to run. 
+# If you'd like to add more jobs, enter them on the following lines. 
+# To help you out, the first command of today's lab is provided:
+bwa aln /proj/g2015009/BB2490/LAB2/all_chr.hg19.fa ERR001014.filt.fastq > ERR001014.filt.bwa_aln.default.sai
